a8d4951e1935eadd5962a828d28d6601a1896e02
Erickson Juang <a820224@gmail.com>
2015-07-26 03:21:41 +0800

    finish main block

--- react_component/MainBlock.jsx
+++ react_component/MainBlock.jsx
@@ -1,14 +1,26 @@
-/*
-MainBlock - by Erickson
-"Key Vision"
-*/
 var MainBlock = React.createClass({
-    render: function() {
-        return (
-            <section className="block" id="MainBlock">ITAC</section>
-        );
-    }
+  render: function() {
+    return (
+      <section className="block" id="MainBlock" data-scroll-index={this.props.scrollIndex}>
+        <div className="ui grid container">
+          <div className="twelve wide column">
+            {/*
+              <p> Device大小：{window.outerWidth} * {window.outerHeight} </p>
+              <p> 視窗大小：{$(window).width()} * {$(window).height()}</p>
+            */}
+            <img className="ui medium image" src="img/ITAC-Logo.png" />
+            <img className="logo-text ui image" src="img/main/itac.png" />
+          </div>
+        </div>
+        <div className="button">
+          <a href="#" data-scroll-nav={parseInt(this.props.scrollIndex) + 1}>
+            <i className="chevron down icon" />
+          </a>
+        </div>
+      </section>
+    );
+  }
 });
 
-//把模組匯出
+
 module.exports = MainBlock;
