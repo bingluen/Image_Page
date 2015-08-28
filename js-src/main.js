53be9ad531c5fd142c0fcbfe10bb52096cf4fb1f
Erickson Juang <a820224@gmail.com>
2015-08-28 19:04:09 +0800

    Modular javascript

---
+++ js-src/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // fix menu when passed
+  $('#MainBlock')
+    .visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      },
+      offset: 40
+    });
+
+  //boots nav
+  $.scrollIt();
+});
