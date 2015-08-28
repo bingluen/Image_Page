342ab10abbaf1ee55962f22a0ba71c6b1c590b36
Erickson Juang <a820224@gmail.com>
2015-08-28 23:00:52 +0800

    add swiper (not finish)

--- js-src/main.js
+++ js-src/main.js
@@ -14,4 +14,20 @@ $(document).ready(function() {
 
   //boots nav
   $.scrollIt();
+
+  //boots swiper
+  var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+        }
+    });
 });
