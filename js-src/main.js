da31a8306bfb3703853d3966478df092357ac2dd
Erickson Juang <a820224@gmail.com>
2015-09-06 11:12:44 +0800

    error swipe function

--- js-src/main.js
+++ js-src/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     // boots scrollIt
     $.scrollIt()
+
+    var slideSwipe = new swipe({
+      container: '.swipe-container',
+      pagination: '.swipe-pagination',
+      buttonPrev: '.swipe-button-prev',
+      buttonNext: '.swipe-button-next'
+    });
+
 });
