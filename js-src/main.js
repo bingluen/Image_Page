63fecb8548f1e1d2c9e67e22d1527d5a00e865cf
Erickson Juang <a820224@gmail.com>
2015-09-06 12:46:36 +0800

    save in local

--- js-src/main.js
+++ js-src/main.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
     // boots scrollIt
     $.scrollIt()
 
-    var swiper = new Swiper('.swiper-container', {
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      loop: true,
-      centeredSlides: true
+    //updateContainerSize
+
+    //$('#driverTaipei').modal('show')
+
+    $('[data-collection]').click(function() {
+      $("#" + $(this).data('collection')).modal('show');
+      var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        loop: true
+      });
     });
 
 });
